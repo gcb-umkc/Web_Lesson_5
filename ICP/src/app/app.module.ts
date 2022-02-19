4f4ec21e6c1179b7fdd34bcfbdcd11f87528d73e
BrahianOsborny <65873402+BrahianOsborny@users.noreply.github.com>
2022-02-19 00:28:30 -0600

    updated project

--- ICP/src/app/app.module.ts
+++ ICP/src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
+
+import { FormsModule } from '@angular/forms'
+import { timerComponent } from './Timer/timer/timer.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    timerComponent
   ],
-    imports: [
-        BrowserModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
